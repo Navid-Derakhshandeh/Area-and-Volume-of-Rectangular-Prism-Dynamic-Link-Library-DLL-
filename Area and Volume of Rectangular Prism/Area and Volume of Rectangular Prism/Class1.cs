381c88e9bf414e9857e11f576fe1c537febd5cd5
Navid-Derakhshandeh <111235264+Navid-Derakhshandeh@users.noreply.github.com>
2022-09-08 20:27:21 +0430

    Add files via upload

---
+++ Area and Volume of Rectangular Prism/Area and Volume of Rectangular Prism/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surface_Area_and_Volume_of_Rectangular_Prism
+{
+    public class Calc
+    {
+        private double SA;
+        private double V;
+
+        public Calc()
+        {
+            Console.WriteLine("Please Enter the Length number of your Rectangular Prism : ");
+            double L = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Width number of your Rectangular Prism : ");
+            double W = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Height number of your Rectangular Prism : ");
+            double H = Double.Parse(Console.ReadLine());
+
+            SA = 2 * (L * W + L * H + W * H);
+
+            Console.WriteLine("Surface Area of Your Rectangular Prism Equal by : {0}.", SA);
+
+            V = L * W * H;
+
+            Console.WriteLine("Volume of Your Rectangular Prism Equal by : {0}.", V);
+        }
+
+    }
+
+
+}
---
+++ Main program/Main program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Surface_Area_and_Volume_of_Rectangular_Prism;
+namespace Main_program 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc obj1 = new Calc();
+
+            Console.ReadLine();
+        }
+    }
+}
